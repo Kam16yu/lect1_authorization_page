1cdde36e365420ccb66f6a22c76fc5321b05131a
Kam16yu <1vsmsh1@gmail.com>
2022-10-22 18:17:03 +0300

    Added splash screen

--- android/app/src/main/kotlin/com/kam16yu/lect1/lect1_page_authorization/MainActivity.kt
+++ android/app/src/main/kotlin/com/kam16yu/lect1/lect1_page_authorization/MainActivity.kt
@@ -1,6 +1,21 @@
 package com.kam16yu.lect1.lect1_page_authorization
 
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import androidx.core.view.WindowCompat
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Aligns the Flutter view vertically with the window.
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // Disable the Android splash screen fade out animation to avoid
+            // a flicker before the similar frame is drawn in Flutter.
+            splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
+        }
+
+        super.onCreate(savedInstanceState)
+    }
 }
